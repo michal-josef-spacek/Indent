6059f77c7864d77183534b4dcadb4d0782ef3765
Michal Špaček <skim@cpan.org>
2005-02-14 13:26:26 +0000

    Scripts.

---
+++ word_with_comments1.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+# $Id: word_with_comments1.pl,v 1.1 2005-02-14 13:26:26 skim Exp $
+
+# Pragmas.
+use strict;
+use warnings;
+
+# Modules.
+use Indent::Word;
+use Indent::Comment;
+use Dumpvalue;
+
+# Objects.
+my $word = new Indent::Word(
+	'indent_len' => 10,
+	'indenter' => '',
+);
+my $comments = new Indent::Comment(
+	'middle' => '# ',
+);
+
+# Indent data.
+my @data = $word->indent('aaaaa bbbbb ddddd eeeee fffff ggggg hhhhh fffff'.
+	' dddd dddd ffff gerg gergrg grgerger grger ');
+
+# Adding comments.
+my @data2 = $comments->indent(\@data);
+
+# Dump data.
+my $dump = new Dumpvalue;
+$dump->dumpValues(@data2);
+
+
+
---
+++ word_with_comments2.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+# $Id: word_with_comments2.pl,v 1.1 2005-02-14 13:26:26 skim Exp $
+
+# Pragmas.
+use strict;
+use warnings;
+
+# Modules.
+use Indent::Word;
+use Indent::Comment;
+use Dumpvalue;
+
+# Objects.
+my $word = new Indent::Word(
+	'indent_len' => 10,
+	'indenter' => '',
+);
+my $comments = new Indent::Comment(
+	'begin' => '/*',
+	'middle' => ' * ',
+	'end' => ' */',
+);
+
+# Indent data.
+my @data = $word->indent('aaaaa bbbbb ddddd eeeee fffff ggggg hhhhh fffff'.
+	' dddd dddd ffff gerg gergrg grgerger grger ');
+
+# Adding comments.
+my @data2 = $comments->indent(\@data);
+
+# Dump data.
+my $dump = new Dumpvalue;
+$dump->dumpValues(@data2);
+
+
+
