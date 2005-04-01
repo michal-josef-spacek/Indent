b90d5708aabe618d91a6f2021334558b3a2fbeee
Michal Špaček <skim@cpan.org>
2005-04-01 11:57:56 +0000

    Optimalization.

--- data.pl
+++ data.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-# $Id: data.pl,v 1.1 2005-02-14 12:13:42 skim Exp $
+# $Id: data.pl,v 1.2 2005-04-01 12:02:07 skim Exp $
 
 # Pragmas.
 use strict;
@@ -8,10 +8,13 @@ use warnings;
 # Modules.
 use Indent::Data;
 
+# Object.
 my $obj = new Indent::Data(
 	'indent_len' => 10,
 	'indenter' => '  ',
 );
+
+# Indent.
 my $data = $obj->indent('dddddddddddddddddddddddddwefefwefewf');
 print "$data\n";
 
