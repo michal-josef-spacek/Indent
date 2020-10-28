ed5f00fd9870470b55b8c06e8c7e024bccba51ac
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-10-28 18:11:55 +0100

    [Common] Improve LICENSE AND COPYRIGHT sections in doc

--- Indent/Utils.pm
+++ Indent/Utils.pm
@@ -260,8 +260,9 @@ L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2005-2020 Michal Josef Špaček
- BSD 2-Clause License
+© 2005-2020 Michal Josef Špaček
+
+BSD 2-Clause License
 
 =head1 VERSION
 
